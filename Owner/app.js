c51a6d6e6df2a4cef7c54d0a6851a1140fba33c3
shahkandarp <102229699+shahkandarp@users.noreply.github.com>
2023-03-05 00:41:47 +0530

    Done with image upload on Firebase

--- Owner/app.js
+++ Owner/app.js
@@ -1,6 +1,17 @@
 require("dotenv").config();
 require("express-async-errors");
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("./errors/index");
+const Owner = require('./models/Owner')
+const Room = require('./models/Room')
+// const multer = require("multer");
+const {
+  ref,
+  uploadBytes,
+  listAll,
+  deleteObject,
+} = require("firebase/storage");
+const storage = require("./firebase");
 // const Interest = require('./models/Interest')
 
 // extra security packages
@@ -49,14 +60,85 @@ app.use(xss());
 // });
 // const upload = multer({ storage: multerStorage })
 
-//routes owner
-app.use('/api/v1/owner',OwnerMiddleware,OwnerRouter)
-
 // app.post('/createInterest',OwnerMiddleware,async(req,res) => {
 //   req.body.ownerId = req.user.ownerId
 //   const interest = await Interest.create(req.body)
 //   res.status(StatusCodes.OK).json({res:'Success',data:interest})
 // })
+
+const memoStorage = multer.memoryStorage();
+const upload = multer({ memoStorage });
+
+app.post("/api/v1/addownerphoto",OwnerMiddleware, upload.single("pic"), async (req, res) => {
+  const {ownerId} = req.user
+  if(!ownerId){
+    throw new BadRequestError("Please provide Owner ID");
+  }
+  const file = req.file;
+  const imageRef = ref(storage, file.originalname);
+  const metatype = { contentType: file.mimetype, name: file.originalname };
+  const snapshot = await uploadBytes(imageRef, file.buffer, metatype)
+  const ownerx = await Owner.findOne({_id:ownerId})
+  ownerx.photos.push(`https://firebasestorage.googleapis.com/v0/b/${snapshot.ref._location.bucket}/o/${snapshot.ref._location.path_}?alt=media`)
+  const owner = await Owner.findOneAndUpdate({_id:ownerId},ownerx,{ runValidators: true, new: true, setDefaultsOnInsert: true })
+  res.status(StatusCodes.OK).json({res:'Success',data:owner})
+});
+
+app.post("/api/v1/addownervideo",OwnerMiddleware, upload.single("pic"), async (req, res) => {
+  const {ownerId} = req.user
+  if(!ownerId){
+    throw new BadRequestError("Please provide Owner ID");
+  }
+  const file = req.file;
+  const imageRef = ref(storage, file.originalname);
+  const metatype = { contentType: file.mimetype, name: file.originalname };
+  const snapshot = await uploadBytes(imageRef, file.buffer, metatype)
+  const ownerx = await Owner.findOne({_id:ownerId})
+  ownerx.videos.push(`https://firebasestorage.googleapis.com/v0/b/${snapshot.ref._location.bucket}/o/${snapshot.ref._location.path_}?alt=media`)
+  const owner = await Owner.findOneAndUpdate({_id:ownerId},ownerx,{ runValidators: true, new: true, setDefaultsOnInsert: true })
+  res.status(StatusCodes.OK).json({res:'Success',data:owner})
+});
+
+app.post("/api/v1/addroomphoto/:rid",OwnerMiddleware, upload.single("pic"), async (req, res) => {
+  const {ownerId} = req.user
+  const {rid} = req.params
+  if(!rid){
+    throw new BadRequestError("Please provide Room ID");
+  }
+  const file = req.file;
+  const imageRef = ref(storage, file.originalname);
+  const metatype = { contentType: file.mimetype, name: file.originalname };
+  const snapshot = await uploadBytes(imageRef, file.buffer, metatype)
+  const roomx = await Room.findOne({_id:rid})
+  if(!roomx){
+    throw new BadRequestError("Please provide Valid Room ID");
+  }
+  roomx.photos.push(`https://firebasestorage.googleapis.com/v0/b/${snapshot.ref._location.bucket}/o/${snapshot.ref._location.path_}?alt=media`)
+  const room = await Room.findOneAndUpdate({_id:rid},roomx,{ runValidators: true, new: true, setDefaultsOnInsert: true })
+  res.status(StatusCodes.OK).json({res:'Success',data:room})
+});
+
+app.post("/api/v1/addroomvideo/:rid",OwnerMiddleware, upload.single("pic"), async (req, res) => {
+  const {ownerId} = req.user
+  const {rid} = req.params
+  if(!rid){
+    throw new BadRequestError("Please provide Room ID");
+  }
+  const file = req.file;
+  const imageRef = ref(storage, file.originalname);
+  const metatype = { contentType: file.mimetype, name: file.originalname };
+  const snapshot = await uploadBytes(imageRef, file.buffer, metatype)
+  const roomx = await Room.findOne({_id:rid})
+  if(!roomx){
+    throw new BadRequestError("Please provide Valid Room ID");
+  }
+  roomx.videos.push(`https://firebasestorage.googleapis.com/v0/b/${snapshot.ref._location.bucket}/o/${snapshot.ref._location.path_}?alt=media`)
+  const room = await Room.findOneAndUpdate({_id:rid},roomx,{ runValidators: true, new: true, setDefaultsOnInsert: true })
+  res.status(StatusCodes.OK).json({res:'Success',data:room})
+});
+
+//routes owner
+app.use('/api/v1/owner',OwnerMiddleware,OwnerRouter)
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -75,6 +157,7 @@ const start = async () => {
     );
   } catch (error) {
     console.log(error);
+
   }
 };
 
